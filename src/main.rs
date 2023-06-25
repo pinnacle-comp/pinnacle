f3331de381cf998cec2188e8445a3508fb564263
Seaotatop <ottatop1227@gmail.com>
2023-06-25 17:52:58 -0500

    Test CI thing

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@
 //! While Pinnacle is not a library, this documentation serves to guide those who want to
 //! contribute or learn how building something like this works.
 
+// test
+
 mod api;
 mod backend;
 mod cursor;
