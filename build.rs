48da903a0ff279555620b158db52d88e3cfc16f7
Ottatop <ottatop1227@gmail.com>
2024-01-08 18:55:59 -0600

    Create proto files

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use const_format::formatcp;
+
 fn main() {
     println!("cargo:rerun-if-changed=api/lua");
     println!("cargo:rerun-if-changed=api/protocol");
@@ -8,4 +10,20 @@ fn main() {
         .unwrap();
 
     tonic_build::compile_protos("api/protocol/request.proto").unwrap();
+
+    const VERSION: &str = "v0alpha1";
+    const PROTOS: &[&str] = &[
+        formatcp!("api/protocol/pinnacle/{VERSION}/pinnacle.proto"),
+        formatcp!("api/protocol/pinnacle/input/{VERSION}/input.proto"),
+        formatcp!("api/protocol/pinnacle/input/libinput/{VERSION}/libinput.proto"),
+        formatcp!("api/protocol/pinnacle/output/{VERSION}/output.proto"),
+        formatcp!("api/protocol/pinnacle/process/{VERSION}/process.proto"),
+        formatcp!("api/protocol/pinnacle/tag/{VERSION}/tag.proto"),
+        formatcp!("api/protocol/pinnacle/window/{VERSION}/window.proto"),
+        formatcp!("api/protocol/pinnacle/window/rules/{VERSION}/rules.proto"),
+    ];
+
+    tonic_build::configure()
+        .compile(PROTOS, &["api/protocol"])
+        .unwrap();
 }
--- src/api/protocol.rs
+++ src/api/protocol.rs
@@ -1,37 +1,41 @@
-use tonic::{Request, Response, Status};
-
-use self::request::{CloseWindowRequest, SetKeybindRequest, SetMousebindRequest};
-
-pub mod request {
-    tonic::include_proto!("request");
-}
-
-#[derive(Default)]
-pub struct CommandServer;
-
-#[tonic::async_trait]
-impl request::command_service_server::CommandService for CommandServer {
-    async fn set_keybind(
-        &self,
-        request: Request<SetKeybindRequest>,
-    ) -> Result<Response<()>, Status> {
-        println!("got set_keybind with {request:?}");
-        Ok(Response::new(()))
+pub mod pinnacle {
+    pub mod v0alpha1 {
+        tonic::include_proto!("pinnacle.v0alpha1");
     }
 
-    async fn set_mousebind(
-        &self,
-        request: Request<SetMousebindRequest>,
-    ) -> Result<Response<()>, Status> {
-        println!("got set_mousebind with {request:?}");
-        Ok(Response::new(()))
+    pub mod input {
+        pub mod v0alpha1 {
+            tonic::include_proto!("pinnacle.input.v0alpha1");
+        }
+
+        pub mod libinput {
+            pub mod v0alpha1 {
+                tonic::include_proto!("pinnacle.input.libinput.v0alpha1");
+            }
+        }
     }
 
-    async fn close_window(
-        &self,
-        request: Request<CloseWindowRequest>,
-    ) -> Result<Response<()>, Status> {
-        println!("got close_window with {request:?}");
-        Ok(Response::new(()))
+    pub mod output {
+        pub mod v0alpha1 {
+            tonic::include_proto!("pinnacle.output.v0alpha1");
+        }
+    }
+
+    pub mod tag {
+        pub mod v0alpha1 {
+            tonic::include_proto!("pinnacle.tag.v0alpha1");
+        }
+    }
+
+    pub mod window {
+        pub mod v0alpha1 {
+            tonic::include_proto!("pinnacle.window.v0alpha1");
+        }
+
+        pub mod rules {
+            pub mod v0alpha1 {
+                tonic::include_proto!("pinnacle.window.rules.v0alpha1");
+            }
+        }
     }
 }
