9ab2820bf21de6b37435276355936d3414b88dd4
nix <rwstauffer72@gmail.com>
2024-04-28 17:52:07 -0400

    run formatter

--- nix/packages/pinnacle-api-lua.nix
+++ nix/packages/pinnacle-api-lua.nix
@@ -1,16 +1,9 @@
-{
-  lua,
-  cqueues,
-  http,
-  compat53,
-  lua-protobuf,
-  buildLuarocksPackage,
-}:
+{ lua, cqueues, http, compat53, lua-protobuf, buildLuarocksPackage, }:
 # need version of http newer than in nixpkgs - check if 0.3 is ok
 buildLuarocksPackage {
   pname = "pinnacle-api";
   version = "dev-1";
 
   src = ../../api/lua;
-  propagatedBuildInputs = [lua cqueues http compat53 lua-protobuf];
+  propagatedBuildInputs = [ lua cqueues http compat53 lua-protobuf ];
 }
