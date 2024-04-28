de625e47c39b8d148a6e9c158ab149648deb20bd
nix <rwstauffer72@gmail.com>
2024-04-28 17:50:17 -0400

    make rust config conform to the same style lua config does

--- nix/lib/buildPinnacleRustConfig.nix
+++ nix/lib/buildPinnacleRustConfig.nix
@@ -1,13 +1,14 @@
 { craneLib, crateArgs, pinnacle-api }:
 { src }:
-(craneLib.buildPackage (crateArgs // {
+(craneLib.buildPackage (crateArgs // rec {
   inherit src;
   inherit (pinnacle-api) cargoArtifacts; # use pinnacle api cargo artifacts
   pname = "pinnacle-config";
   installPhaseCommand = ''
-    mkdir -p $out/bin/pinnacle-config
-    ls
-    mv target/release/pinnacle-config $out/bin/pinnacle-config
-    mv metaconfig.toml $out/bin/pinnacle-config
+    mkdir -p $out/share/pinnacle/config/
+    mkdir $out/bin
+    mv target/release/${pname} $out/bin/
+    mv metaconfig.toml $out/share/pinnacle/config/
+    ln $out/bin/${pname} $out/share/pinnacle/config/${pname};
   '';
 }))
