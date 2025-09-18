b009ffef6e3793ee9504694ee0d803d6f309985f
Cassandra Comar <cass@ndra.io>
2025-09-18 08:58:31 -0400

    minor cleanup

--- src/handlers/ext_workspace.rs
+++ src/handlers/ext_workspace.rs
@@ -1,20 +1,23 @@
+use smithay::output::Output;
+
 use crate::delegate_ext_workspace;
 use crate::output::OutputName;
-use crate::protocol::ext_workspace::{self, ExtWorkspaceHandler};
+use crate::protocol::ext_workspace::{ExtWorkspaceHandler, ExtWorkspaceManagerState};
 use crate::state::State;
+use crate::tag::TagId;
 
 impl ExtWorkspaceHandler for State {
-    fn ext_workspace_manager_state(&mut self) -> &mut ext_workspace::ExtWorkspaceManagerState {
+    fn ext_workspace_manager_state(&mut self) -> &mut ExtWorkspaceManagerState {
         &mut self.pinnacle.ext_workspace_state
     }
 
-    fn activate_workspace(&mut self, id: crate::tag::TagId) {
+    fn activate_workspace(&mut self, id: TagId) {
         if let Some(tag) = id.tag(&self.pinnacle) {
             crate::api::tag::switch_to(self, &tag);
         }
     }
 
-    fn assign_workspace(&mut self, id: crate::tag::TagId, output: smithay::output::Output) {
+    fn assign_workspace(&mut self, id: TagId, output: Output) {
         if let Some(tag) = id.tag(&self.pinnacle) {
             crate::api::tag::add(self, Some(tag.name()), OutputName(output.name()));
         }
