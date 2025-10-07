e8d74315b211a09b9a619260ba5dbb26ee71be1a
phantomas <phantomas@phantomas.xyz>
2025-10-08 01:10:46 +0200

    snowcap/lua: add TextInput widget

--- snowcap/api/lua/snowcap/widget.lua
+++ snowcap/api/lua/snowcap/widget.lua
@@ -31,6 +31,7 @@
 ---@field image snowcap.widget.Image?
 ---@field input_region snowcap.widget.InputRegion?
 ---@field mouse_area snowcap.widget.MouseArea?
+---@field text_input snowcap.widget.TextInput?
 
 ---@class snowcap.widget.Border
 ---@field color snowcap.widget.Color?
@@ -271,6 +272,170 @@ local mouse = {
     },
 }
 
+---A field that can be filled with text.
+---
+---## Example
+---Create a simple Layer with an automatically focused `TextInput`:
+---
+---```lua
+---local Layer = require("snowcap.layer")
+---local Operation = require("snowcap.widget.operation")
+---local Widget = require("snowcap.widget")
+---
+---local TextInputProgram = {
+---    INPUT_ID = "prompt",
+---}
+---
+---function TextInputProgram:view()
+---    return Widget.text_input({
+---        placeholder = "placeholder:",
+---        value = self.input_value or "",
+---        id = self.INPUT_ID,
+---        on_input = function(data) return { content_changed = data } end,
+---        on_submit = { submit = {} },
+---        width = Widget.length.Fixed(500.0)
+---    })
+---end
+---
+---function TextInputProgram:update(msg)
+---    if msg.content_changed then
+---        self.input_value = msg.content_changed
+---    elseif msg.submit then
+---        -- do something with the input.
+---        self.input_value = ""
+---    end
+---end
+---
+---function TextInputProgram:show()
+---    local handle = Layer.new_widget({
+---        program = self,
+---        anchor = nil,
+---        keyboard_interactivity = Layer.keyboard_interactivity.EXCLUSIVE,
+---        exclusive_zone = "respect",
+---        layer = Layer.zlayer.OVERLAY,
+---    })
+---    if not handle then return end
+---
+---    -- Focus the input
+---    handle:operate(Operation.focusable.Focus(self.INPUT_ID))
+---    handle:on_key_press(function(_, key)
+---        local Keys = require("snowcap.input.keys")
+---        if key == Keys.Escape then handle:close() end
+---        if key == Keys.i then
+---            handle:operate(Operation.focusable.Focus(self.INPUT_ID))
+---        end
+---    end)
+---end
+---
+---function text_input_program()
+---    local instance = {
+---        INPUT_ID = TextInputProgram.INPUT_ID,
+---        input_value = "",
+---    }
+---    setmetatable(instance, { __index = TextInputProgram })
+---    return instance
+---end
+---```
+---@class snowcap.widget.TextInput
+---Text to display when the field is empty.
+---@field placeholder string
+---TextInput content.
+---@field value string
+---Set the TextInput Id.
+---
+---This id can then be used to target this widget with `Operation`s.
+---@field id string?
+---Convert the `TextInput` into a secure password input.
+---@field secure boolean?
+---Sets the message that should be produced when some text is typed into the `TextInput`.
+---
+---If the field is not set, the `TextInput` will be disabled.
+---@field on_input (fun(data:string): any)?
+---Sets the message that should be produced when the `TextInput` is focused and the enter
+---key is pressed.
+---@field on_submit any?
+---Sets the message that should be produced when some text is pasted into the `TextInput`.
+---@field on_paste (fun(data:string): any)?
+---Sets the `Font` of the `TextInput`.
+---@field font snowcap.widget.Font?
+---Sets the `Icon` of the `TextInput`.
+---@field icon snowcap.widget.text_input.Icon?
+---Sets the width of the `TextInput`.
+---@field width snowcap.widget.Length?
+---Sets the `Padding` of the `TextInput`.
+---@field padding snowcap.widget.Padding?
+---Sets the `LineHeight` of the `TextInput`.
+---@field line_height snowcap.widget.LineHeight?
+---Sets the horizontal `Alignment` of the `TextInput`.
+---@field horizontal_alignment snowcap.widget.Alignment?
+---Sets the style of the `TextInput`.
+---@field style snowcap.widget.text_input.Styles?
+---@field package widget_id integer?
+
+---The `TextInput` callbacks.
+---@class snowcap.widget.text_input.Callbacks
+---Sets the message that should be produced when some text is typed into the `TextInput`.
+---
+---If the field is not set, the `TextInput` will be disabled.
+---@field on_input (fun(data:string): any)?
+---Sets the message that should be produced when the `TextInput` is focused and the enter
+---key is pressed.
+---@field on_submit any?
+---Sets the message that should be produced when some text is pasted into the `TextInput`.
+---@field on_paste (fun(data:string): any)?
+
+---The content of the `Icon`.
+---@class snowcap.widget.text_input.Icon
+---The `Font` that will be used to display the `code_point`.
+---@field font snowcap.widget.Font?
+---The unicode code point that will be used as the icon.
+---@field code_point integer?
+---The font size of the content.
+---@field pixels number?
+---The spacing between the `Icon` and the text in a `TextInput`.
+---@field spacing number?
+---Whether the icon should be displayed on the right side of the `TextInput`.
+---@field right_side boolean?
+
+---Styles to apply to the `TextInput`.
+---@class snowcap.widget.text_input.Styles
+---Style to use when the `TextInput` is active.
+---@field active snowcap.widget.text_input.Style?
+---Style to use when the `TextInput` is hovered.
+---@field hovered snowcap.widget.text_input.Style?
+---Style to use when the `TextInput` is focused.
+---@field focused snowcap.widget.text_input.Style?
+---Style to use when the `TextInput` is focused & hovered.
+---@field hover_focused snowcap.widget.text_input.Style?
+---Style to use when the `TextInput` is disabled.
+---@field disabled snowcap.widget.text_input.Style?
+
+---Appearance of a `TextInput`.
+---@class snowcap.widget.text_input.Style
+---The `Background` style.
+---@field background snowcap.widget.Background?
+---The `Border` of the `TextInput`.
+---@field border snowcap.widget.Border?
+---The `Color` of the `Icon`.
+---@field icon snowcap.widget.Color?
+---The `Color` of the placeholder.
+---@field placeholder snowcap.widget.Color?
+---The `Color` of the content.
+---@field value snowcap.widget.Color?
+---The `Color` to use for the selection's highlight.
+---@field selection snowcap.widget.Color?
+
+---@class snowcap.widget.text_input.Event
+---@field event_type snowcap.widget.text_input.event.Type?
+---@field data string?
+
+---@enum snowcap.widget.text_input.event.Type
+local text_input_event_type = {
+    INPUT = "input",
+    SUBMIT = "submit",
+    PASTE = "press",
+}
+
 ---@class snowcap.widget.Length
 ---@field fill {}?
 ---@field fill_portion integer?
@@ -349,6 +514,30 @@ local background = {
         local gradient = { linear = linear }
 
         return { gradient = gradient }
+    end,
+}
+
+---The height of a line of text in a paragraph.
+---@class snowcap.widget.LineHeight
+---A factor of the size of the text.
+---@field relative number?
+---An absolute height in logical pixels.
+---@field absolute number?
+
+---Builders for `LineHeight`
+---@class snowcap.widget.line_height
+---Builds a relative `LineHeight`.
+---@field Relative fun(size: number): snowcap.widget.LineHeight
+---Builds an absolute `LineHeight`.
+---@field Absolute fun(size: number): snowcap.widget.LineHeight
+local line_height = {
+    ---@type fun(size: number): snowcap.widget.LineHeight
+    Relative = function(size)
+        return { relative = size }
+    end,
+    ---@type fun(size: number): snowcap.widget.LineHeight
+    Absolute = function(size)
+        return { absolute = size }
     end,
 }
 
@@ -450,6 +639,7 @@ local font = {
 ---@class snowcap.widget.Callback
 ---@field button fun(widget: snowcap.widget.WidgetDef)?
 ---@field mouse_area fun(widget: snowcap.widget.WidgetDef)?
+---@field text_input fun(widget: snowcap.widget.WidgetDef)?
 
 local widget = {
     length = length,
@@ -460,6 +650,7 @@ local widget = {
     image = {
         content_fit = content_fit,
     },
+    line_height = line_height,
     mouse = mouse,
 }
 
@@ -618,6 +809,29 @@ local function mouse_area_into_api(def)
     }
 end
 
+---@param def snowcap.widget.TextInput
+---@return snowcap.widget.v1.TextInput
+local function text_input_into_api(def)
+    ---@type snowcap.widget.v1.TextInput
+    return {
+        placeholder = def.placeholder,
+        value = def.value,
+        id = def.id,
+        secure = def.secure,
+        on_input = def.on_input ~= nil,
+        on_submit = def.on_submit ~= nil,
+        on_paste = def.on_paste ~= nil,
+        font = def.font --[[@as snowcap.widget.v1.Font]],
+        icon = def.icon --[[@as snowcap.widget.v1.TextInput.Icon]],
+        width = def.width --[[@as snowcap.widget.v1.Length]],
+        padding = def.padding --[[@as snowcap.widget.v1.Padding]],
+        line_height = def.line_height --[[@as snowcap.widget.v1.LineHeight]],
+        horizontal_alignment = def.horizontal_alignment --[[@as snowcap.widget.v1.Alignment]],
+        style = def.style --[[@as snowcap.widget.v1.TextInput.Style]],
+        widget_id = def.widget_id,
+    }
+end
+
 ---@param def snowcap.widget.WidgetDef
 ---@return snowcap.widget.v1.WidgetDef
 function widget.widget_def_into_api(def)
@@ -647,6 +861,9 @@ function widget.widget_def_into_api(def)
     end
     if def.mouse_area then
         def.mouse_area = mouse_area_into_api(def.mouse_area)
+    end
+    if def.text_input then
+        def.text_input = text_input_into_api(def.text_input)
     end
 
     return def --[[@as snowcap.widget.v1.WidgetDef]]
@@ -762,6 +979,28 @@ function widget.mouse_area(mouse_area)
     }
 end
 
+---Create a new TextInput widget.
+---@param text_input snowcap.widget.TextInput
+---
+---@return snowcap.widget.WidgetDef
+function widget.text_input(text_input)
+    local has_cb = false
+
+    has_cb = has_cb or text_input.on_input ~= nil
+    has_cb = has_cb or text_input.on_submit ~= nil
+    has_cb = has_cb or text_input.on_paste ~= nil
+
+    if has_cb then
+        text_input.widget_id = widget_id_counter
+        widget_id_counter = widget_id_counter + 1
+    end
+
+    ---@type snowcap.widget.WidgetDef
+    return {
+        text_input = text_input,
+    }
+end
+
 ---@private
 ---@lcat nodoc
 ---@param wgt snowcap.widget.WidgetDef
@@ -812,6 +1051,20 @@ local function collect_mouse_area_callbacks(mouse_area)
     }
 end
 
+---@package
+---@lcat nodoc
+---
+---Collect event callbacks from a `snowcap.widget.TextInput`
+---@param text_input snowcap.widget.TextInput
+---@return snowcap.widget.text_input.Callbacks
+local function collect_text_input_callbacks(text_input)
+    return {
+        on_input = text_input.on_input,
+        on_submit = text_input.on_submit,
+        on_paste = text_input.on_paste,
+    }
+end
+
 ---@private
 ---@lcat nodoc
 ---@param callbacks any[]
@@ -823,6 +1076,10 @@ function widget._collect_callbacks(callbacks, wgt)
 
     if wgt.mouse_area and wgt.mouse_area.widget_id then
         callbacks[wgt.mouse_area.widget_id] = collect_mouse_area_callbacks(wgt.mouse_area)
+    end
+
+    if wgt.text_input and wgt.text_input.widget_id then
+        callbacks[wgt.text_input.widget_id] = collect_text_input_callbacks(wgt.text_input)
     end
 end
 
@@ -890,6 +1147,53 @@ end
 
 ---@private
 ---@lcat nodoc
+---@param callbacks snowcap.widget.text_input.Callbacks
+---@param event snowcap.widget.text_input.Event
+---@return any?
+function widget._text_input_process_event(callbacks, event)
+    callbacks = callbacks or {}
+
+    local translate = {
+        [text_input_event_type.INPUT] = "on_input",
+        [text_input_event_type.SUBMIT] = "on_submit",
+        [text_input_event_type.PASTE] = "on_paste",
+    }
+
+    local event_type = nil
+    local cb = nil
+
+    for k, v in pairs(translate) do
+        if event[k] ~= nil then
+            event_type = k
+            cb = callbacks[v]
+
+            break
+        end
+    end
+
+    if cb == nil then
+        return nil
+    end
+
+    local msg = nil
+
+    if event_type == text_input_event_type.SUBMIT then
+        msg = cb
+    else
+        local ok, val = pcall(cb, event[event_type])
+
+        if not ok then
+            require("snowcap.log").error(val)
+        else
+            msg = val
+        end
+    end
+
+    return msg
+end
+
+---@private
+---@lcat nodoc
 ---@param callbacks any[]
 ---@param event snowcap.widget.v1.WidgetEvent
 function widget._message_from_event(callbacks, event)
@@ -902,6 +1206,11 @@ function widget._message_from_event(callbacks, event)
         if callbacks[widget_id] ~= nil then
             ---@diagnostic disable-next-line:param-type-mismatch
             msg = widget._mouse_area_process_event(callbacks[widget_id], event.mouse_area)
+        end
+    elseif event.text_input then
+        if callbacks[widget_id] ~= nil then
+            ---@diagnostic disable-next-line:param-type-mismatch
+            msg = widget._text_input_process_event(callbacks[widget_id], event.text_input)
         end
     end
 
