1f38b6d0be05f4a4ef6c07b93379089351208049
phantomas <phantomas@phantomas.xyz>
2025-10-26 16:57:50 +0100

    snowcap: rust: Add a method to get unprocessed key event
    The on_key_pressed handler can be limiting since:
    - it doesn't allow to react to key being released
    - it doesn't transmit the processed `text` associated with keys when
      using a layout with dead keys.
    - it doesn't transmit captured events (I've added this one to conserve
      the previous behavior.)
    
    This commit address this by adding a on_key_event that transmit the key
    state, whether it was captured on the server side, and the associated
    text if available.

--- snowcap/api/rust/src/input.rs
+++ snowcap/api/rust/src/input.rs
@@ -1,10 +1,11 @@
 //! Input types.
 
 use snowcap_api_defs::snowcap::input;
+use xkbcommon::xkb::Keysym;
 
 /// Keyboard modifiers.
 #[allow(missing_docs)]
-#[derive(Default)]
+#[derive(Default, Debug, Clone, Copy)]
 pub struct Modifiers {
     pub shift: bool,
     pub ctrl: bool,
@@ -19,6 +20,33 @@ impl From<input::v1::Modifiers> for Modifiers {
             ctrl: value.ctrl,
             alt: value.alt,
             logo: value.super_,
+        }
+    }
+}
+
+/// A Key event.
+#[derive(Debug, Clone)]
+pub struct KeyEvent {
+    /// Key Symbol.
+    pub key: Keysym,
+    /// Currently active modifiers.
+    pub mods: Modifiers,
+    /// True if the key is currently pressed, false on release.
+    pub pressed: bool,
+    /// True if the event was flagged as Captured by a widget.
+    pub captured: bool,
+    /// Text produced by the event, if any.
+    pub text: Option<String>,
+}
+
+impl From<input::v1::KeyboardKeyResponse> for KeyEvent {
+    fn from(value: input::v1::KeyboardKeyResponse) -> Self {
+        Self {
+            key: Keysym::new(value.key),
+            mods: Modifiers::from(value.modifiers.unwrap_or_default()),
+            pressed: value.pressed,
+            captured: value.captured,
+            text: value.text,
         }
     }
 }
