a69742f0a29229f45a3f2af23fa4711497eb722d
phantomas <phantomas@phantomas.xyz>
2025-12-18 23:37:49 +0100

    snowcap: rust: Support Popup
    changes:
    - Initial implementation
    - Support nested popups
    - Popup positioning
    - Gravity & Anchor
    - Offset
    - Position popup at widget
    - Keyboard events for popups
    - Make constraint adjustment configurable
    - Allow updating positioner attributes
    - Support widget operations

--- snowcap/api/rust/src/layer.rs
+++ snowcap/api/rust/src/layer.rs
@@ -19,6 +19,7 @@ use crate::{
     BlockOnTokio,
     client::Client,
     input::{KeyEvent, Modifiers},
+    popup::{self, AsParent},
     widget::{self, Program, WidgetDef, WidgetId, WidgetMessage},
 };
 
@@ -383,5 +384,11 @@ where
 
             on_press(handle, event.key, event.mods)
         });
+    }
+}
+
+impl<Msg> AsParent for LayerHandle<Msg> {
+    fn as_parent(&self) -> crate::popup::Parent {
+        popup::Parent(popup::ParentInner::Layer(self.id))
     }
 }
