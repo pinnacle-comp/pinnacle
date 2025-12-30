1457ce64f56f267b0b488e9850e4c5e2f166607c
Ottatop <ottatop1227@gmail.com>
2025-12-29 19:20:09 -0600

    hack: Use focused output scale as fallback for initial fractional scale
    So firefox 146 released recently, bringing with it fractional scale support. In classic firefox fashion, there are a few, let's just say, *quirks* with the implementation, notably regarding popups. Our current fractional scale implementation, for the most part, sends the fractional scale for the first output. If this is different from the scale of the output the popup will actually show up on, the popup will flicker at the wrong scale for a frame. Also, the positioning becomes completely thrown off. Doesn't happen on any other application, just firefox. This is a small hack to get that to stop.
    
    Also that firefox update causes popups to not show up if I turn my monitor off and on, that's fun. No idea what's causing that. I've reproduced it on jay and cosmic but not on niri

--- src/handlers.rs
+++ src/handlers.rs
@@ -631,20 +631,8 @@ delegate_viewporter!(State);
 
 impl FractionalScaleHandler for State {
     fn new_fractional_scale(&mut self, surface: WlSurface) {
-        let _span = tracy_client::span!("FractionalScaleHandler::new_fractional_scale");
+        let _span = tracy_client::span!();
 
-        // comment yanked from anvil
-        // Here we can set the initial fractional scale
-        //
-        // First we look if the surface already has a primary scan-out output, if not
-        // we test if the surface is a subsurface and try to use the primary scan-out output
-        // of the root surface. If the root also has no primary scan-out output we just try
-        // to use the first output of the toplevel.
-        // If the surface is the root we also try to use the first output of the toplevel.
-        //
-        // If all the above tests do not lead to a output we just use the first output
-        // of the space (which in case of anvil will also be the output a toplevel will
-        // initially be placed on)
         let mut root = surface.clone();
         while let Some(parent) = compositor::get_parent(&root) {
             root = parent;
@@ -677,7 +665,10 @@ impl FractionalScaleHandler for State {
                             })
                         }
                     })
-                    .or_else(|| self.pinnacle.space.outputs().next().cloned());
+                    .or_else(|| {
+                        // FIXME: proper notification of scale and transform
+                        self.pinnacle.focused_output().cloned()
+                    });
             if let Some(output) = primary_scanout_output {
                 fractional_scale::with_fractional_scale(states, |fractional_scale| {
                     fractional_scale.set_preferred_scale(output.current_scale().fractional_scale());
