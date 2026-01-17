d600a0fdc13c836ac241250ec432cf9f82632a5e
phantomas <phantomas@phantomas.xyz>
2026-01-17 09:31:13 +0100

    snowcap: Add backround for widget

--- snowcap/src/util/convert.rs
+++ snowcap/src/util/convert.rs
@@ -3,3 +3,9 @@
 pub trait FromApi<T> {
     fn from_api(api_type: T) -> Self;
 }
+
+pub trait TryFromApi<T>: Sized {
+    type Error;
+
+    fn try_from_api(api_type: T) -> Result<Self, Self::Error>;
+}
