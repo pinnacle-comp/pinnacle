f6f6b346380049545e6af7afdfb39ff018dad0c2
Ottatop <ottatop1227@gmail.com>
2026-01-24 13:27:38 -0600

    snowcap(lua): Make program composable

--- snowcap/api/lua/snowcap/widget.lua
+++ snowcap/api/lua/snowcap/widget.lua
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
----@class snowcap.widget.Program
+---@class snowcap.widget.Program : snowcap.widget.base.Base
 ---@field update fun(self: self, message: any)
 ---@field view fun(self: self): snowcap.widget.WidgetDef
 
