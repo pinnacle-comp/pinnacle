855c73f097a43a2bd9ed1eee792c30331422795a
Ottatop <ottatop1227@gmail.com>
2026-02-10 17:44:29 -0600

    snowcap(rust): Provide `is` and `downcast` methods for `UniversalMsg`

--- snowcap/api/rust/src/widget/message.rs
+++ snowcap/api/rust/src/widget/message.rs
@@ -3,9 +3,41 @@ use std::any::Any;
 /// A universal message type.
 ///
 /// This is a suitable catch-all message for all built-in widget programs.
-/// Types must implement [`Universal`] in order to be converted to and from this message.
+/// Types must implement [`Universal`] in order to be converted into and from this message.
 #[derive(Debug, Clone)]
 pub struct UniversalMsg(Box<dyn Value>);
+
+impl UniversalMsg {
+    /// Returns `true` if this `UniversalMsg` is of type `T`.
+    pub fn is<T: 'static>(&self) -> bool {
+        (&*self.0 as &dyn Any).is::<T>()
+    }
+
+    /// Attempts to downcast this `UniversalMsg` to a concrete type.
+    pub fn downcast<T: 'static>(self) -> Result<T, Self> {
+        let value = self.0;
+        if (&*value as &dyn Any).is::<T>() {
+            let ret = (value as Box<dyn Any>)
+                .downcast::<T>()
+                .expect("checked above");
+            Ok(*ret)
+        } else {
+            Err(Self(value))
+        }
+    }
+
+    /// Returns a reference to the inner value of it is of type `T`,
+    /// or `None` if it isn't.
+    pub fn downcast_ref<T: 'static>(&self) -> Option<&T> {
+        (&*self.0 as &dyn Any).downcast_ref()
+    }
+
+    /// Returns a mutable reference to the inner value of it is of type `T`,
+    /// or `None` if it isn't.
+    pub fn downcast_mut<T: 'static>(&mut self) -> Option<&mut T> {
+        (&mut *self.0 as &mut dyn Any).downcast_mut()
+    }
+}
 
 trait Value: dyn_clone::DynClone + Any + Send {}
 dyn_clone::clone_trait_object!(Value);
@@ -18,25 +50,26 @@ impl std::fmt::Debug for dyn Value {
     }
 }
 
-/// A trait that allows types to be converted to and from the [`UniversalMsg`].
+/// A trait that allows types to be converted into and from the [`UniversalMsg`].
 pub trait Universal: Clone + Send + 'static {
-    fn to_universal(self) -> UniversalMsg {
+    // Converts this type into a `UniversalMsg`.
+    fn into_universal(self) -> UniversalMsg {
         UniversalMsg(Box::new(self) as _)
     }
-    fn from_universal(msg: UniversalMsg) -> Option<Self> {
-        let msg = msg.0 as Box<dyn Any>;
-        msg.downcast::<Self>().ok().map(|inner| *inner)
+    // Attempts to convert a `UniversalMsg` into this type.
+    fn from_universal(msg: UniversalMsg) -> Result<Self, UniversalMsg> {
+        msg.downcast()
     }
 }
 
 impl<T: Universal> From<T> for UniversalMsg {
     fn from(value: T) -> Self {
-        value.to_universal()
+        value.into_universal()
     }
 }
 
 impl<T: Universal> From<UniversalMsg> for Option<T> {
     fn from(value: UniversalMsg) -> Self {
-        T::from_universal(value)
+        T::from_universal(value).ok()
     }
 }
